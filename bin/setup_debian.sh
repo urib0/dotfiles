58ce4320aeb7d194b74243a1f012fe3948de4532
uribo <ur1b0.dev@gmail.com>
2022-09-04 01:59:42 +0900

    ログ出力を追加

--- bin/setup_debian.sh
+++ bin/setup_debian.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# ログ出力の設定
+LOG_OUT=./$(date +"log_%Y%m%d_%H%M%S%m.txt")
+
+exec 1> >(tee -a $LOG_OUT)
+exec 2>>$LOG_OUT
+# 将来的にログだけタイムスタンプつけるようにしたい
+#exec 1> >(
+#  while read -r l; do echo "[$(date +"%Y-%m-%d %H:%M:%S")] $l"; done \
+#    | tee -a $LOG_OUT
+#)
+
 sudo printf ''
 
 sudo apt update
