292bd324ab17d38cf5b00ace67a72dc7782b716a
uribo <ur1b0.dev@gmail.com>
2022-10-18 15:43:55 +0900

    [mac] .bash_profileを追加

--- bin/step_symlink.sh
+++ bin/step_symlink.sh
@@ -2,6 +2,9 @@ cd ~
 if ! [ -f ".bash_aliases" ]; then
   ln -s ./dotfiles/.bash_aliases .
 fi
+if ! [ -f ".bash_profile" ]; then
+  ln -s ./dotfiles/.bash_profile .
+fi
 if ! [ -f ".aliases" ]; then
   ln -s ./dotfiles/.aliases .
 fi
