09d8b5569dc9fff18075b6708ab34431b4773529
uribo <ur1b0.dev@gmail.com>
2022-10-18 20:29:41 +0900

    [mac] .bash_profileを読んでpyenvが入っているか確認する処理を入れた

--- bin/step_pip.sh
+++ bin/step_pip.sh
@@ -1,3 +1,13 @@
-pip install --upgrade pip
-pip install powerline-shell
-pip install wheel
+
+echo $(pip -V)
+echo "source ~./.bash_profile"
+source ~/.bash_profile
+echo $(pip -V)
+
+if [ -v PYENV_ROOT ]; then
+  pip install --upgrade pip
+  pip install powerline-shell
+  pip install wheel
+else
+  echo "PYENV_ROOT is not defined"
+fi
