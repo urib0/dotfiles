52d99970407213016ef2462d6890d100b002e8f7
uribo <ur1b0.dev@gmail.com>
2022-11-14 00:28:12 +0900

    add cmake,ccmake

--- bin/step_apt.sh
+++ bin/step_apt.sh
@@ -1,6 +1,8 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y git
+sudo apt install -y cmake
+sudo apt install -y cmake-curses-gui
 sudo apt install -y tmux
 sudo apt install -y avahi-daemon
 sudo apt install -y openssh-server
